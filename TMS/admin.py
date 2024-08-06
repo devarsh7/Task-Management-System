0dfcd8a9a12d86193174d81a29dc7a50de98bddb
Devarsh7 <devarsh.shahs2000@gmail.com>
2024-08-06 01:43:53 -0400

    Updated the admin rights

--- TMS/admin.py
+++ TMS/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Task, Notification, Message
+from .models import Task
 
 admin.site.register(Task)
-admin.site.register(Notification)
-admin.site.register(Message)
+
