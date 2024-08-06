2ef0b17b713d7a8d4c87ad0b3dc6a4d2fe5d14a3
Devarsh7 <devarsh.shahs2000@gmail.com>
2024-08-06 19:03:18 -0400

    Added comments in the code

--- TMS/api_endpoints.py
+++ TMS/api_endpoints.py
@@ -6,26 +6,35 @@ from rest_framework import status
 from .models import Task
 from .serializers import TaskSerializer
 
+
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-
+    
+    # This method will allow user to create a new task
+    # It returns the serialized data of newly created task with 201 created status
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+    # This method will allow user to view all tasks
+    # It returns the serialized data of all Task instances with 200 OK status
     def list(self, request, *args, **kwargs):
         queryset = self.get_queryset()
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+    # This method will allow user to view tasks with specific id
+    # It returns the serialized data with 200 OK status
     def retrieve(self, request, pk=None, *args, **kwargs):
         task = self.get_object()
         serializer = self.get_serializer(task)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+    # This method will allow user to update the tasks with specific id
+    # It returns the serialized data with 200 OK status
     def update(self, request, pk=None, *args, **kwargs):
         partial = kwargs.pop('partial', False)
         instance = self.get_object()
@@ -34,6 +43,8 @@ class TaskViewSet(viewsets.ModelViewSet):
         self.perform_update(serializer)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+    # This method will allow user to delete a task with specific id
+    # It returns a 204 no content status stating successful deletion
     def destroy(self, request, pk=None, *args, **kwargs):
         instance = self.get_object()
         self.perform_destroy(instance)
