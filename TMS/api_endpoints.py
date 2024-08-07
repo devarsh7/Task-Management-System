19d5c63a57be47902616825d87a334bde2009482
Devarsh7 <devarsh.shahs2000@gmail.com>
2024-08-07 01:50:11 -0400

    Added JWT auth for API endpoints

--- TMS/api_endpoints.py
+++ TMS/api_endpoints.py
@@ -1,16 +1,17 @@
 # api_endpoints.py
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from rest_framework import status
 from .models import Task
 from .serializers import TaskSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-    
+    permission_classes = [IsAuthenticated]
+
     # This method will allow user to create a new task
     # It returns the serialized data of newly created task with 201 created status
     def create(self, request, *args, **kwargs):
