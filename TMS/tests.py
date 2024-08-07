ed37d9b466673589bb3a4604059cd7201e6cc388
Devarsh7 <devarsh.shahs2000@gmail.com>
2024-08-07 02:27:57 -0400

    Updated test cases

--- TMS/tests.py
+++ TMS/tests.py
@@ -1,36 +1,67 @@
-"""
-from rest_framework.test import APITestCase, APIClient
+from django.contrib.auth.models import User
 from rest_framework import status
+from rest_framework.test import APITestCase
 from django.urls import reverse
 from .models import Task
-from django.contrib.auth.models import User
+from .serializers import TaskSerializer
+
 
 class TaskAPITestCase(APITestCase):
+    # Method will initialize test data before each test method
     def setUp(self):
-        self.client = APIClient()
-        self.create_url = reverse('task_list')  # Make sure 'task-list' matches the name in your URL conf
-        # Create a user and authenticate
-        self.user = User.objects.create_user(username='testuser', password='testpass')
-        self.client.force_authenticate(user=self.user)
-        self.user.id = 6
-
-    def test_create_task(self):
-        data = {
-            "title": "Test Task",
-            "assigned_to": self.user.id  # Use the id of the created user
+        self.user = User.objects.create_user(username='devarsh', password='admin123')
+        self.task1 = Task.objects.create(title="Task 1", assigned_to=self.user)
+        self.task2 = Task.objects.create(title="Task 2", assigned_to=self.user)
+        self.valid_payload = {
+            'title': 'New Task',
+            'assigned_to': self.user.id
         }
-        response = self.client.post(self.create_url, data, format='json')
+        self.invalid_payload = {
+            'title': '',
+            'assigned_to': self.user.id
+        }
+
+    # Test case to check weather new task is created or not
+    def test_create_task(self):
+        url = reverse('task_list')
+        response = self.client.post(url, data=self.valid_payload, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Task.objects.count(), 3)
 
-# Ensure you have a URL pattern for the TaskViewSet in your urls.py file like so:
-# from django.urls import path, include
-# from rest_framework.routers import DefaultRouter
-# from .api_views import TaskViewSet
+    # Test case to check weather new task is created with invalid data
+    def test_create_task_invalid(self):
+        url = reverse('task_list')
+        response = self.client.post(url, data=self.invalid_payload, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-# router = DefaultRouter()
-# router.register(r'tasks', TaskViewSet, basename='task')
+    # Test case to fetch all the tasks
+    def test_list_tasks(self):
+        url = reverse('task_list')
+        response = self.client.get(url, format='json')
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
 
-# urlpatterns = [
-#     path('', include(router.urls)),
-# ]
-"""
+    # Test case to check to fetch a given task with id
+    def test_retrieve_task(self):
+        url = reverse('task-detail', kwargs={'pk': self.task1.pk})
+        response = self.client.get(url, format='json')
+        serializer = TaskSerializer(self.task1)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
+
+    # Test case to update an existing task of that id
+    def test_update_task(self):
+        url = reverse('task-detail', kwargs={'pk': self.task1.pk})
+        response = self.client.put(url, data=self.valid_payload, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.task1.refresh_from_db()
+        self.assertEqual(self.task1.title, self.valid_payload['title'])
+
+    # Test case to delete any task of the given id
+    def test_delete_task(self):
+        url = reverse('task-detail', kwargs={'pk': self.task1.pk})
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Task.objects.count(), 1)
